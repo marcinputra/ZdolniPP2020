9e10681b2edaaee208569fb82c00edd62bf3712f
Tomasz Hresiukiewicz <tomahu111@gmail.com>
2020-09-18 18:51:56 +0200

    Transparent rectangles(in file rysowanie.h) - "rectangle" changed to draw 4 lines instead of basic rectangle.

--- Projekt/rysowanie.h
+++ Projekt/rysowanie.h
@@ -19,5 +19,10 @@ void rectangle(HDC _hdc, int x1, int y1, int x2, int y2, HPEN kolor)
 {
     SelectObject(_hdc, kolor);
     //MoveToEx(_hdc, x1, y1, NULL);
-    Rectangle(_hdc, x1, y1, x2, y2);
+    //Rectangle(_hdc, x1, y1, x2, y2);
+    MoveToEx(_hdc, x1, y1, NULL);
+    LineTo(_hdc, x1, y2);
+    LineTo(_hdc, x2, y2);
+    LineTo(_hdc, x2, y1);
+    LineTo(_hdc, x1, y1);
 }
