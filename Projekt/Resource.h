a2e21ec923277264bb877760a3545f61176cddeb
BuildTools <unconfigured@null.spigotmc.org>
2020-09-21 15:00:28 +0200

    Revert "Added a menu"
    This reverts commit 2ae341d829a3d066c9443b396ca1a155d3b1973e.

--- Projekt/Resource.h
+++ Projekt/Resource.h
@@ -1,28 +1,30 @@
 ﻿//{{NO_DEPENDENCIES}}
-// Plik dołączany wygenerowany przez środowisko Microsoft Visual C++.
-// Używany przez: Projekt.rc
-//
-#define IDC_MYICON                      2
-#define IDD_PROJEKT_DIALOG              102
-#define IDS_APP_TITLE                   103
-#define IDD_ABOUTBOX                    103
-#define IDM_ABOUT                       104
-#define IDM_EXIT                        105
-#define IDI_PROJEKT                     107
-#define IDI_SMALL                       108
-#define IDC_PROJEKT                     109
-#define IDR_MAINFRAME                   128
-#define ID_WIDOK_ROZMIAROKNA            32771
-#define IDC_STATIC                      -1
+// Plik dołączany wygenerowany przez program Microsoft Visual C++.
+// Używane przez: Projekt.rc
 
-// Next default values for new objects
-// 
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_PROJEKT_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_PROJEKT			107
+#define IDI_SMALL				108
+#define IDC_PROJEKT			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Następne wartości domyślne dla nowych obiektów
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
-#define _APS_NEXT_COMMAND_VALUE         32772
-#define _APS_NEXT_CONTROL_VALUE         1003
-#define _APS_NEXT_SYMED_VALUE           110
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
 #endif
 #endif
